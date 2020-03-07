aaac099a91bed5416a1bbd50d17d44b73c2c22be
Alexander Gauggel <agauggel@uni-koblenz.de>
2020-03-07 12:11:26 +0100

    removed unused functions

--- src/CML/Common.h
+++ src/CML/Common.h
@@ -3,7 +3,5 @@
 #include "Vector.h"
 
 namespace cml {
-	float lerp(const float a, const float b, const float t);
-	cml::vec3 lerp(const cml::vec3& a, const cml::vec3& b, const float t);
 	float radian(const float degree);
 }
